b88a63737540fefbbc935ba7a24475602318f351
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2021-11-02 12:36:02 +0900

    fix
    足し算を忘れていました

--- lesson/1-9.py
+++ lesson/1-9.py
@@ -7,7 +7,7 @@ for i in sample:
     sample_sam+=-i*math.log(i,2)
 print(sample_sam)
 for k in sample2:
-    sample_sam2=-k*math.log(k,2)
+    sample_sam2+=-k*math.log(k,2)
 print(sample_sam2)
 print(sample_sam>sample_sam2)
 # 上記の結果から平均情報量が小さくなり、予測が簡単なことがわかる
