7ea847845d2df5151adaaef53f39a61e4939bd39
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2021-11-04 12:17:35 +0900

    Update 4-4.py

--- lesson-4/4-4.py
+++ lesson-4/4-4.py
@@ -29,24 +29,3 @@ for i in range(0,10):
                print(i*k,end=" ")
     #改行
     print("")
-
-    # for i in range(0,10):
-    # for k in range(1,10):
-    #     #上の方に何の段を表示する
-    #     if i==0:
-    #         #kが１である時最初に空白を入れる
-    #         if k==1:
-    #             print(end="    ")
-    #         #数字を入れていく
-    #         print(k,end="  ")
-    #     #最初に何の段を表示する
-    #     if k==1 and i>0:
-    #         print(f"{i}: ",end=" ")
-    #         print(i*k,end="  ")
-    #     #1桁の時は空白二個分,2桁は空白１つ分で表を整える
-    #     if k>=2 and i>0:
-    #         if i*k<10 and i>0:
-    #            print(i*k,end="  ")
-    #         else:
-    #            print(i*k,end=" ")
-    # print("")
