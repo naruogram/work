3e9747758214b7c7afae69079c99789a17b4493a
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2021-11-05 14:28:38 +0900

    fix

--- lesson-5/5-1.py
+++ lesson-5/5-1.py
@@ -24,13 +24,14 @@ for j in range(2,b):
         k-=1
 datas2.append(math.floor(b))
 b=98
+#素因数分解の結果を使って、最大公約数を計算
 for q in datas:
     for w in datas2:
         if q==w and q!=0:
             data=data*q
             datas.remove(q)
             datas2.remove(w)
-            datas.insert(0,"0")
-            datas2.insert(0,"0")
+            datas.insert(0,0)
+            datas2.insert(0,0)
             break
 print(f"{a}と{b}の最大公約数は{data}")
