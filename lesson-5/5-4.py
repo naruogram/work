f20a9f5fcc0f470cfa2f29b9d01cb4e66b283cc0
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2021-12-05 19:23:47 +0900

    fix

--- lesson-5/5-4.py
+++ lesson-5/5-4.py
@@ -3,14 +3,9 @@ LIMIT = 10**-20
 def f(x):
     return x*x - 2
 
-xn1 = float(input("xn1を入力してください"))
-while f(xn1)<0:
-    xn1 = float(input("xn1を入力してください"))
-    continue
-xn0 = float(input("xn0を入力してください"))
-while f(xn0)>0:
-    xn0 = float(input("xn0を入力してください"))
-    continue
+xn1 = 2
+xn0 = 1
+
 
 while (xn1 - xn0) * (xn1 - xn0) > LIMIT:
     xm = (xn1 + xn0) / 2
@@ -18,4 +13,4 @@ while (xn1 - xn0) * (xn1 - xn0) > LIMIT:
         xn1 = xm
     else:
         xn0 = xm
-    print(f"近似値は{(xn1+xn0)/2}")
+print(f"近似値は{(xn1+xn0)/2}")
